6b69f8ec2b850de14a3f0c3e16763d0005771282
Comit <priyanshusengar2001@gmail.com>
2026-09-05 23:50:52 +0530

    Complete Assignment 5 - Canteen Payment Dispatch

---
+++ Assignments/A5_CanteenPaymentDispatch/Main.java
@@ -0,0 +1,50 @@
+class Payment {
+
+    double pay(double amount) {
+        System.out.println("Paid (cash): Rs " + amount);
+        return amount;
+    }
+}
+
+class CardPayment extends Payment {
+
+    double payWithProcessingFee(double amount) {
+        double total = amount * 1.02;
+        System.out.println("Charged (card, incl. fee): Rs " + total);
+        return total;
+    }
+}
+
+public class Main {
+
+    static double processTransaction(Payment payment, double amount) {
+
+        if (payment instanceof CardPayment) {
+            CardPayment card = (CardPayment) payment;
+            return card.payWithProcessingFee(amount);
+        } else {
+            return payment.pay(amount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Payment[] payments = {
+                new CardPayment(),
+                new Payment(),
+                new CardPayment(),
+                new Payment(),
+                new CardPayment()
+        };
+
+        double[] amounts = {100, 50, 200, 75, 120};
+
+        double totalCollected = 0;
+
+        for (int i = 0; i < payments.length; i++) {
+            totalCollected += processTransaction(payments[i], amounts[i]);
+        }
+
+        System.out.println("Total Collected: Rs " + totalCollected);
+    }
+}
